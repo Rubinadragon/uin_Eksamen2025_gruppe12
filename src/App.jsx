ed6609133d6c6558f2251624974cc4c5bf802242
JulieAnd3 <julia@hiof.no>
2025-04-25 21:51:19 +0200

    Satt opp routing
    Satte opp Home og Event page opp med en overskrift .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Layout from "./components/Layout"
+import Dashboard from "./components/Dashboard"
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import CategoryPage from './components/CategoryPage'
+import EventPage from './components/EventPage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <Layout>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path="event/:id" element={<EventPage/>}/>
+          <Route path="category/:slug" element={<CategoryPage/>}/>
+          <Route path="dashboard" element={<Dashboard/>}/>
+        </Routes>
+     </Layout>
     </>
   )
 }

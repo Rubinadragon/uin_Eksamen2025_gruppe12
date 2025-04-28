d5b16d43638ac591aa93e9fa622af02cc79025c9
Rubinadragon <cecilive@hiof.no>
2025-04-28 22:04:43 +0200

    Kommentarer til videre arbeid

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,11 @@ import { Link, NavLink } from "react-router-dom";
 import LinkButton from "./LinkButton";
 
 export default function Header(){
+    //Get classification details
+    //segment, _embedded,
+    //Musikk: KZFzniwnSyZfZ7v7nJ
+    //Sport: KZFzniwnSyZfZ7v7nE
+    //Fant ikke noe som het teater/show, men isteden fant jeg Arts & Theatre: KZFzniwnSyZfZ7v7na
     return (<header>
                 <section className="headerSection">
                     <Link to="/" className="logo">Billettlyst</Link>

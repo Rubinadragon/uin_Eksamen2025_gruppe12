d7884fa92a72c3730ef1def980a93142d8c61302
Rubinadragon <cecilive@hiof.no>
2025-04-29 10:06:35 +0200

    Tror jeg er ferdig med fetch, TODO: mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Layout from "./components/Layout"
 import Dashboard from "./components/Dashboard"
 import { Route, Routes } from 'react-router-dom'
@@ -7,10 +7,25 @@ import CategoryPage from './components/CategoryPage'
 import EventPage from './components/EventPage'
 
 function App() {
+  const [selectedClasses, setSelectedClasses] = useState([])
 
+  const apiKey = "sV6gYIGVOW7z9DLVElsxVgGUyC5Ox3EX";
+
+  const getSelectedClasses = async () => {
+    await fetch(`https://app.ticketmaster.com/discovery/v2/classifications?apikey=${apiKey}&id=KZFzniwnSyZfZ7v7nJ,KZFzniwnSyZfZ7v7nE,KZFzniwnSyZfZ7v7na&locale=no`)
+    .then((response) => response.json())
+    .then((data) => setSelectedClasses(data._embedded.classifications))
+    
+  }
+
+  useEffect(()=>{
+    getSelectedClasses()
+  },[])
+
+  console.log(setSelectedClasses)
   return (
     <>
-     <Layout>
+     <Layout selectedClasses={selectedClasses} setSelectedClasses={setSelectedClasses}>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path="event/:id" element={<EventPage/>}/>

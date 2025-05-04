53ebfe774c7f827f98bc6ce65cbe7038ea39748d
Kjell-Magne <kjell_m.larsen@yahoo.com>
2025-05-04 23:09:52 +0200

    Feilhåndtering av category fetch og tilpasset landskoder til Ticketmaster

--- src/components/CategoryPage.jsx
+++ src/components/CategoryPage.jsx
@@ -8,43 +8,29 @@ export default function CategoryPage({ selectedClasses }) {
     const { slug } = useParams();
 
     const [categorySuggestions, setCategorySuggestions] = useState([]);
-    const [attractions, setAttractions] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [loadingResults, setLoadingResults] = useState(true);
 
     const handleSubmit = async (e) => {
       e.preventDefault();
+      setLoadingResults(true)
       await getSuggestionsFilter(e.target.filterCountries.value, slug)
     }
 
     const getSuggestionsFilter = async(countryCode, segmentId) => {
       let apidata = null;
-      let eventData = null;
       await fetch(`https://app.ticketmaster.com/discovery/v2/suggest?apikey=sV6gYIGVOW7z9DLVElsxVgGUyC5Ox3EX&locale=*&countryCode=${countryCode}&segmentId=${segmentId}`)
       .then((response) => response.json())
-      .then((data) => (apidata = data._embedded, eventData = data._embedded.events))
+      .then((data) => (apidata = data._embedded))
 
       setCategorySuggestions(apidata)
-      getAttractionsFromEvents(eventData)
-    }
-
-    const getAttractionsFromEvents = (values) => {
-      let allAttractions = values.map((value) => value._embedded.attractions[0])
-      
-      let uniqie = allAttractions.reduce((acc, obj) => {
-        if(!acc.some(o => o.id === obj.id)) {
-          acc.push(obj);
-        }
-        return acc;
-      }, [])
-      setAttractions(uniqie)
-
+      setLoadingResults(false)
     }
 
     const getSuggestionsByCategory = async(keyword) => {
       try {
         const data = await fetchSuggestions(keyword);
         setCategorySuggestions(data);
-        setAttractions(data.attractions)
+        setLoadingResults(false);
       }
       catch(error) {
         console.log(error);
@@ -62,6 +48,7 @@ export default function CategoryPage({ selectedClasses }) {
     
     const categoryName = selectedClasses.find(cls => cls.segment.id === slug)?.segment.name;
 
+    
     return (
       <>
         <h1>Kategori: {categoryName}</h1>
@@ -82,49 +69,55 @@ export default function CategoryPage({ selectedClasses }) {
         <section>
           <h2>Attractions</h2>
           {
-            categorySuggestions.events?.reduce((acc, obj) => {
-              if(!acc.some(o => o._embedded.attractions[0].id === obj._embedded.attractions[0].id)) {
+            !loadingResults && "events" in categorySuggestions ?
+            categorySuggestions.events.reduce((acc, obj) => {
+              if(!acc.some(o => o._embedded?.attractions?.[0].id === obj._embedded?.attractions?.[0].id)) {
                 acc.push(obj)
               }
               return acc
               }, []).map((attraction) => (
                 <article key={`categoryAttraction_${attraction.id}`}>
                 <img src={
-                  attraction.images.find((img) => img.ratio === "16_9" && (img.width < 800 && img.width > 300)).url
+                  "images" in attraction ? attraction.images.find((img) => img.ratio === "16_9" && (img.width < 800 && img.width > 300)).url : "test.jpg"
                 } alt="" />
                 <h3>{attraction.name}</h3>
               </article>
             ))
+            :
+            <p>Ingen attraksjoner funnet</p>
           }
         </section>
         <section>
           <h2>Events</h2>
           {
-            categorySuggestions.events?.map((event) => (
-              <article key={`categoryEvent_${event.id}`}>
-              <img src={
-                event.images.find((element) => element.ratio === "16_9" && (element.width < 800 && element.width > 300)).url
-              } alt="" />
-              <h3>{event.name}</h3>
-            </article>
-            ))
+            !loadingResults && "events" in categorySuggestions ?
+              categorySuggestions.events?.map((event) => (
+                <article key={`categoryEvent_${event.id}`}>
+                <img src={
+                 "images" in event ? event.images.find((element) => element.ratio === "16_9" && (element.width < 800 && element.width > 300)).url : "test.jpg"
+                } alt="" />
+                <h3>{event.name}</h3>
+              </article>
+              ))
+              : <p>Ingen arrangementer funnet</p>
           }
         </section>
         <section>
           <h2>Venues</h2>
           {
-            categorySuggestions.venues?.map((venue) => (
-              <article key={`categoryVenue${venue.id}`}>
-              {
-                'images' in venue ? 
-                <img src={
-                venue.images.find((element) => element.ratio === "16_9").url 
-              } alt="" />
-              : ""
-              }
-              <h3>{venue.name}</h3>
-            </article>
-            ))
+            !loadingResults && "venues" in categorySuggestions ?
+              categorySuggestions.venues?.map((venue) => (
+                <article key={`categoryVenue${venue.id}`}>
+                {
+                  <img src={
+                  "images" in venue ? venue.images.find((element) => element.ratio === "16_9").url : "test.jpg"
+                } alt="" />
+                }
+                <h3>{venue.name}</h3>
+              </article>
+              ))
+              :
+              <p>Ingen lokasjoner funnet</p>
           }
         </section>
   

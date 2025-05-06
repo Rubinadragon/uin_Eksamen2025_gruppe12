bd568e98814bc539d3850464e6a99681cbbdf03f
Kjell-Magne <kjell_m.larsen@yahoo.com>
2025-05-06 15:47:22 +0200

    Filtrering med søk, flyttet filtrering i Filter komponent

--- src/components/CategoryPage.jsx
+++ src/components/CategoryPage.jsx
@@ -1,82 +1,41 @@
 import { useParams }   from "react-router-dom";
 import { useState, useEffect } from "react";
 import { fetchSuggestions } from "../fetchers/fetchTicketmaster";
+import Filter from "./Filter";
 import "../assets/styles/categoryPage.scss";
-import {countries} from "../assets/js/countryCodes";
-import { cities } from "../assets/js/citiesLocation";
 
 export default function CategoryPage({ selectedClasses }) {
     const { slug } = useParams();
 
     const [categorySuggestions, setCategorySuggestions] = useState([]);
     const [loadingResults, setLoadingResults] = useState(true);
-    const [filterCountry, setFilterCountry] = useState("");
+    const [filterQuery, setFilterQuery] = useState([]);
 
     useEffect(() => {
-      getSuggestionsFilter(slug);
-    }, [slug]);
+      getSuggestions(slug, filterQuery);
+    }, [slug, filterQuery]);
 
-    const getSuggestionsFilter = async(segmentId, e = {}) => {
-      let buildFilter = "";
-      if(segmentId.length)
-        buildFilter += "&segmentId=" + segmentId;
-      if("target" in e && e.target.filterCountries.value.length)
-        buildFilter += "&countryCode=" + e.target.filterCountries.value;
-      if("target" in e && e.target.filterCities.value.length)
-        buildFilter += "&geoPoint=" + e.target.filterCities.value;
-      if("target" in e && e.target.filterDates.value.length)
-        buildFilter += "&localStartEndDateTime=" + e.target.filterDates.value + "T00:00:00";
+    const getSuggestions = async(segmentId, e) => {
+      let query = `&segmentId=${segmentId}${e}`;
       
-      console.log(buildFilter)
       try {
-        const data = await fetchSuggestions(buildFilter);
-        setCategorySuggestions(data);
+        const data = await fetchSuggestions(query);
+
+        data ? setCategorySuggestions(data) : setCategorySuggestions({})
         setLoadingResults(false);
       }
       catch(error) {
         console.log(error);
       }
     }
-
-    const handleChange = (value) => {
-      setFilterCountry(value)
-    }
-
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      setLoadingResults(true)
-      
-      await getSuggestionsFilter(slug, e)
-    }
     
-    const categoryName = selectedClasses.find(cls => cls.segment.id === slug)?.segment.name;
-    
+    const currentCategory = selectedClasses.find(cls => cls.segment.id === slug)?.segment;
+
     return (
       <>
-        <h1>Kategori: {categoryName}</h1>
-        <section>
-          <h2>Filter</h2>
-          <form onSubmit={handleSubmit}>
-            <input type="date" id="filterDates" name="dates" />
-            <select name="countries" id="filterCountries" onChange={(e)=> handleChange(e.target.value)}>
-              <option value="">Velg land</option>
-            {
-              countries.map((country) => (
-                <option key={`select_${country.code}`}value={country.code}>{country.name}</option>
-              ))
-            }
-            </select>
-            <select name="cities" id="filterCities">
-              <option value="">velg by</option>
-              {
-                cities.filter((city) => city.code === filterCountry).map((city) =>(
-                  <option key={`city_${city.name}`} value={city.lat + "," + city.long}>{city.name}</option>
-                ))
-              }
-            </select>
-            <button type="submit">Filtrer søk</button>
-          </form>
-        </section>
+        <h1>Kategori: {currentCategory?.name}</h1>
+
+        <Filter setLoadingResults={setLoadingResults} setFilterQuery={setFilterQuery}/>
 
         <section>
           <h2>Attractions</h2>
@@ -132,17 +91,6 @@ export default function CategoryPage({ selectedClasses }) {
               <p>Ingen lokasjoner funnet</p>
           }
         </section>
-  
-        {/*<div className="eventList">
-          {events.map(evt => (
-            <div key={evt.id}>
-              <h3>{evt.name}</h3>
-              <p>{evt.dates?.start?.localDate}</p>
-              <p>{evt._embedded?.venues?.[0]?.name}</p>
-            </div>
-          ))}
-        </div>
-        */}
         </>
     );
   }

d76f9423ead1de9439758b6c0cc2e36f3eb7e74a
Rubinadragon <cecilive@hiof.no>
2025-05-15 17:27:42 +0200

    Fetch av ønskeliste

--- src/fetchers/eventServices.js
+++ src/fetchers/eventServices.js
@@ -20,5 +20,45 @@ export async function fetchSingleSanityEvent(apiId){
             category
             }`,{apiId})
     return data
-    
 }
+
+//FetchUserByWishList
+export async function FetchUserByWishList(wishlisted){
+    const data = await client.fetch(
+        `*[_type == "event" /&& apiId == $wishlisted{
+            _id,
+            tittel,
+            apiId,
+            "wishlisted":*[_type=='bruker' && ^._id in wishlist[]._ref]{
+                _id,
+                name,
+            }
+        }`, {apiId}
+    )
+}
+
+/*export async function fetchPerson(personSlug) {
+    const data = await client.fetch(
+        `*[personslug.current == $personSlug] | {
+        _id,
+        _createdAt,
+        profilbilde {asset->{url}, alt},
+        personnavn,
+        epost,
+        bio,
+        interesser,
+        "personslug": personslug.current,
+        "personlogg": *[_type=='logg' && references(^._id)]{
+          _id,
+          _createdAt,
+          loggdato,
+          loggpersoner[]-> {
+            personnavn
+          },
+          loggbeskrivelse,
+          loggtimer,
+        } | order(loggdato asc, _createdAt asc)
+    }`, {personSlug}
+    );
+    return data[0];
+}*/

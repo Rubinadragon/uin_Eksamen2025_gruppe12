6d87d9d82dbe5f062fc0ba64157d79d125771699
Rubinadragon <cecilive@hiof.no>
2025-05-16 19:49:05 +0200

    Innlogginsskjerm sass

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,8 +1,7 @@
 import { loadEventImg } from "../assets/js/utils";
+import "../assets/styles/artistCard.scss";
 
 export default function ArtistCard({artist, isProfile = false}){
-  console.log(artist?.image?.asset?.url)
-
     return(
         <article className="artistCard">
             <img src= {isProfile ? artist?.image?.asset?.url : loadEventImg(artist, 300, 800)} alt={`${artist.name} banner`}/>

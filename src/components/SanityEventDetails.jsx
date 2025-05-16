4e55009314869243bd3396413c0c2b974d91facd
Rubinadragon <cecilive@hiof.no>
2025-05-16 22:17:35 +0200

    Begynt sass på sanityEventPage

--- src/components/SanityEventDetails.jsx
+++ src/components/SanityEventDetails.jsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchSingleSanityEvent, fetchUserByWishList} from "../fetchers/eventServices";
 import { fetchSingleEventsById } from "../fetchers/fetchTicketmaster";
-import { loadEventImg } from "../assets/js/utils";
+import { loadEventImg, formatDateNO } from "../assets/js/utils";
 import ArtistCard from "./ArtistCard";
+import "../assets/styles/sanityEventDetails.scss"
 
 export default function SanityEventDetails(){
     let { apiId } = useParams() // Manglet paranteser
@@ -39,21 +40,17 @@ export default function SanityEventDetails(){
     }
 
     console.log(apiEvent)
-    return (<section className="SanityEventDetails">
+    return (<section className="sanityEventDetails">
             <img src={loadEventImg(apiEvent, 600, 2500)} alt={`${apiEvent?.name} banner`}/>
             <h1>{apiEvent.name}</h1>
-            <article>
-                <h2>Dato og sted</h2>
-                <p>Dato: <span>{apiEvent?.dates?.start?.localDate}</span></p>
-                <p>Sted: <span>{apiEvent?._embedded?.venues[0]?.name}</span></p>
+            <article className="sanityEventInfo">
+                <p className="sanityGenre">{apiEvent?.classifications?.[0]?.genre?.name}</p>
+                <p className="sanityDate">{apiEvent?.dates?.start?.localDate}</p>
+                <p className="sanityVenue">{apiEvent?._embedded?.venues[0]?.name}</p>
             </article>
-            <article>
-                <h2>Sjanger</h2>
-                <p>{apiEvent?.classifications?.[0]?.genre?.name}</p>
-            </article>
-            <article>
+            <section className="sanityWishlist">
                 <h2>Hvem har dette i ønskelisten</h2>
                 {wishlistPeople?.[0]?.wishlisted?.map((person) => <ArtistCard key={person._id} artist={person} isProfile={true}/>)}
-            </article>
+            </section>
         </section>)
 }

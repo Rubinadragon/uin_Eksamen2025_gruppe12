ff11f25756223689c4c3644791f70a3875d7e381
Robertjohiof <robertjo@hiof.no>
2025-05-18 13:44:43 +0200

    B-Krav
    KOmmentert ut kode fra B-krav

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,6 +5,21 @@ import UserDashboardView from "./UserDashboardView";
 
 export default function Dashboard({ setIsLoggedIn, setCurrentUser, currentUser, wishlist, setWishlist }) {
 
+  //B-KRAV
+  // const [allUsers, setAllUsers] = useState([]);
+  // const [allEvents, setAllEvents] = useState([]);
+
+  // B-KRAV 
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const users = await fetchAllUsers();
+  //     setAllUsers(users);
+  //     const events = await fetchAllEvents();
+  //     setAllEvents(events);
+  //   };
+  //   fetchData();
+  // }, []);
+
   const handleLogin = async (x) => {
     x.preventDefault();
     const username = x.target.usernameInput.value;
@@ -28,6 +43,34 @@ export default function Dashboard({ setIsLoggedIn, setCurrentUser, currentUser, 
   return (
     currentUser ?
       <UserDashboardView currentUser={currentUser} wishlist={wishlist} setWishlist={setWishlist} />
+      /* B-KRAV
+    <section>
+      <h3>Alle events i Sanity:</h3>
+      <ul>
+        {allEvents.map(evnt => (
+          <li key={evnt.id}> {evnt.tittel} ({evnt.category}) </li>
+        ))}
+      </ul>
+
+      <h3>Alle brukere i Sanity:</h3>
+      {allUsers.map(user => (
+        <div key={user._id}>
+          <p>{user.name}</p>
+          <img src={user.image.asset.url} alt={user.image.alt} />
+          <p>Wishlist: {user.wishlist.length} events </p>
+          <p>Purchases: {user.previousPurchases.length} events </p>
+          <ul>
+            {user.wishlist.map(wish => (
+              <li key={wish._id}>Wishlist: {wish.tittel} </li>
+            ))}
+            {user.previousPurchases.map(purchase => (
+              <li key={purchase._id}>Purchased: {purchase.tittel} </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </section>
+    */
       : (
           <section className="notLoggedIn">
             <h2>Logg inn</h2>
@@ -39,5 +82,5 @@ export default function Dashboard({ setIsLoggedIn, setCurrentUser, currentUser, 
                 <button>Logg inn</button>
             </form>           
           </section>
-  ))
+  ));
 }

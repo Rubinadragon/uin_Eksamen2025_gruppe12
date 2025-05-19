f0fb8f2ebb3cf990aa301d8ab58b1d54bd2bd65f
Kjell-Magne <kjell_m.larsen@yahoo.com>
2025-05-19 10:54:12 +0200

    Test deploy files

--- src/fetchers/fetchTicketmaster.js
+++ src/fetchers/fetchTicketmaster.js
@@ -1,7 +1,6 @@
 export const BASE_URL = "https://app.ticketmaster.com/discovery";
 export const API_VERSION = "v2";
-export const API_KEY = "JF1iWmRvlI6x3AbIps1uDqKtG9njUcTx";
-//export const API_KEY = "sV6gYIGVOW7z9DLVElsxVgGUyC5Ox3EX";
+export const API_KEY = "sV6gYIGVOW7z9DLVElsxVgGUyC5Ox3EX";
 
 // Fetches multiple classifications based on IDS
 // param: String Ids separated with comma (in same string)
